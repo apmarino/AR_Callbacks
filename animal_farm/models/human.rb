e93161a923155e9fee04d078b8ff218a23cfbe2f
Alexander Marino <marinoalexp@gmail.com>
2015-08-26 10:53:14 -0400

    added calls for human

--- animal_farm/models/human.rb
+++ animal_farm/models/human.rb
@@ -2,10 +2,19 @@ require_relative('../lib/connection')
 
 class Person < ActiveRecord::Base
   before_validation :is_old, on: [:create, :update]
-  
+  after_destroy :humans_gone
   private
   def is_old
-    self.name = self.name < " old"
+    if self.age > 20
+      self.name = self.name << " old"
+      binding.pry
+    end
   end
-  
+
+  def humans_gone
+    if Person.all == []
+      Farm.delete_all
+    end
+  end
+
 end
