570f7f36f9c664e009e6f44c6478af2f97ea3d28
Alexander Marino <marinoalexp@gmail.com>
2015-08-26 10:37:45 -0400

    Added callback for human

--- animal_farm/models/human.rb
+++ animal_farm/models/human.rb
@@ -1,5 +1,11 @@
 require_relative('../lib/connection')
 
 class Person < ActiveRecord::Base
-
+  before_validation :is_old, on: [:create, :update]
+  
+  private
+  def is_old
+    self.name = self.name < " old"
+  end
+  
 end
